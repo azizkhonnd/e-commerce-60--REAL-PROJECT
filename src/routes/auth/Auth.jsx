4cced037c53c3548ff0d1e37622977ef0a959cc1
azizkhonnd <azikklv@gmail.com>
2024-07-22 14:16:01 +0500

    initial commit

--- src/routes/auth/Auth.jsx
+++ src/routes/auth/Auth.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const Auth = () => {
   return (
     <div className='w-full h-screen flex items-center justify-center'>
-        <div className='p-[24px] w-[350px]   shadow-cm'>
+      <div className='p-[24px] w-[350px]   shadow-cm'>
         <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-          <Outlet/>
+          <Outlet />
         </GoogleOAuthProvider>
-        
-        </div>
+
+      </div>
     </div>
   )
 }
